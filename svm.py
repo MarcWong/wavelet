cfbfa1e7bd164386c4a127aa0e6bf7ab565defe1
MarcWong <brucewayne@pku.edu.cn>
2019-03-04 12:01:11 +0800

    update statistic script

--- svm.py
+++ svm.py
@@ -18,8 +18,10 @@ def split_X(data, abnormal_rate):
 
 def doSvm(ABNORMAL_RATE, XMIN, YMIN, XMAX, YMAX):
     ####### 文件读取 #######
-    X_train = np.load('../data/simulate/X_train.npy')[:, np.array([0,2])] 
-    X_test = np.load('../data/simulate/X_test.npy')[:, np.array([0,2])] 
+    # X_train = np.load('../data/simulate/X_train.npy')[:, np.array([0,2])] 
+    # X_test = np.load('../data/simulate/X_test.npy')[:, np.array([0,2])] 
+    X_train = np.load('../data/simulate/X_train.npy')
+    X_test = np.load('../data/simulate/X_test.npy')
     Y_train = np.load('../data/simulate/Y_train.npy')
     Y_test = np.load('../data/simulate/Y_test.npy')
 
@@ -51,46 +53,56 @@ def doSvm(ABNORMAL_RATE, XMIN, YMIN, XMAX, YMAX):
     n_error_test = y_pred_test[y_pred_test == 1].size
 
     # 统计正常训练集上的表现
-    cntRegular = 0
+    TP = 0
+    TN = 0
+    FP = 0
+    FN = 0
     for i in range(0, TEST):
         # 判断预测值与真值是否一致
-        if (Y_test[i] == y_pred_test[i]):
-            cntRegular += 1
+        if (Y_test[i] == y_pred_test[i] == 0):
+            TP += 1
+        elif (Y_test[i] == y_pred_test[i] == 1):
+            TN += 1
+        elif (Y_test[i] != y_pred_test[i] and Y_test[i] == 1):
+            FP += 1
+        else:
+            FN += 1
 
+    print ("test set recall: ", TP/ (TP+FN),"; precision:", TP/ (TP+FP), "F1-score:", 2*TP/(2*TP + FP + FN))
     ####### 画图 #######
     # 网格的粒度是第三个参数
-    xx, yy = np.meshgrid(np.linspace(XMIN, XMAX, 1000), np.linspace(YMIN, YMAX, 1000))
+    # xx, yy = np.meshgrid(np.linspace(XMIN, XMAX, 1000), np.linspace(YMIN, YMAX, 1000))
 
     # plot the line, the points, and the nearest vectors to the plane
-    Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
-    plt.figure("svm")
-    plt.title("2-D SVM")
-    plt.contourf(xx, yy, Z, levels=np.linspace(Z.min(), 0, 7), cmap=plt.cm.PuBu)
-    a = plt.contour(xx, yy, Z, levels=[0], linewidths=2, colors='darkred')
-    plt.contourf(xx, yy, Z, levels=[0, Z.max()], colors='palevioletred')
+    # Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+    # Z = Z.reshape(xx.shape)
+    # plt.figure("svm")
+    # plt.title("2-D SVM")
+    # plt.contourf(xx, yy, Z, levels=np.linspace(Z.min(), 0, 7), cmap=plt.cm.PuBu)
+    # a = plt.contour(xx, yy, Z, levels=[0], linewidths=2, colors='darkred')
+    # plt.contourf(xx, yy, Z, levels=[0, Z.max()], colors='palevioletred')
 
-    b1 = plt.scatter(X_train_normal[:, 0], X_train_normal[:, 1], c='white', s=s, edgecolors='k')
-    b2 = plt.scatter(X_train_abnormal[:, 0], X_train_abnormal[:, 1], s=s)
-    b3 = plt.scatter(X_test_normal[:, 0], X_test_normal[:, 1], s=s)
-    b4 = plt.scatter(X_test_abnormal[:, 0], X_test_abnormal[:, 1], s=s)
+    # b1 = plt.scatter(X_train_normal[:, 0], X_train_normal[:, 1], c='white', s=s, edgecolors='k')
+    # b2 = plt.scatter(X_train_abnormal[:, 0], X_train_abnormal[:, 1], s=s)
+    # b3 = plt.scatter(X_test_normal[:, 0], X_test_normal[:, 1], s=s)
+    # b4 = plt.scatter(X_test_abnormal[:, 0], X_test_abnormal[:, 1], s=s)
 
-    plt.axis('tight')
+    # plt.axis('tight')
 
     # X, Y的显示上下界在这里修改
-    plt.xlim((XMIN, XMAX))
-    plt.ylim((YMIN, YMAX))
-    plt.legend([a.collections[0], b1, b2, b3, b4],
-            ["learned frontier", "normal train", "abnormal train",
-                "normal test", "abnormal test",],
-            loc="upper left",
-            prop=matplotlib.font_manager.FontProperties(size=8))
-    plt.xlabel(
-        "error train: %d/%d ; errors test: %d/%d ; "
-        % (n_error_train, TRAIN, n_error_test, TEST))
+    # plt.xlim((XMIN, XMAX))
+    # plt.ylim((YMIN, YMAX))
+    # plt.legend([a.collections[0], b1, b2, b3, b4],
+    #         ["learned frontier", "normal train", "abnormal train",
+    #             "normal test", "abnormal test",],
+    #         loc="upper left",
+    #         prop=matplotlib.font_manager.FontProperties(size=8))
+    # plt.xlabel(
+    #     "error train: %d/%d ; errors test: %d/%d ; "
+    #     % (n_error_train, TRAIN, n_error_test, TEST))
 
-    plt.text(
-        -0.1, -0.8,
-        "test set correctness: %.2f"
-        % (cntRegular / TEST))
-    plt.show()
+    # plt.text(
+    #     -0.1, -0.8,
+    #     "test set correctness: %.2f"
+    #     % (cntRegular / TEST))
+    # plt.show()
