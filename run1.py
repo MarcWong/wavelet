5c614be26efb85386ef898c0a81cae7c07a3efcb
diana <yao.staufenberg@gmail.com>
2019-03-05 20:17:23 +0800

    run1.py

--- run1.py
+++ run1.py
@@ -7,8 +7,8 @@ Created on Mon Mar  4 12:10:32 2019
 """
 
 print(__doc__)
-from data_generation import generateData
-from svm import doSvm
+from data_generation1 import generateData
+from svm1 import doSvm
 
 ####### 一些参数 #######
 TRAIN = 5000
@@ -19,8 +19,8 @@ ABNORMAL_RATE = 0.1
 MIU = 0
 SIGMA = 0.1
 # 异常数据的均值、方差
-MIU_ABNORMAL = 0.1
-SIGMA_ABNORMAL = 0.4
+MIU_ABNORMAL = 0.5
+SIGMA_ABNORMAL = 0.1
 # 画图时的上下界
 XMIN = -3.0
 YMIN = -3.0
@@ -30,5 +30,5 @@ YMAX = 3.0
 # 小波变换的级数
 LEVEL = 2
 
-generateData(TRAIN, TEST, ABNORMAL_RATE, MIU, SIGMA, MIU_ABNORMAL, SIGMA_ABNORMAL, LEVEL)
+generateData(TRAIN, TEST, ABNORMAL_RATE, MIU, SIGMA, MIU_ABNORMAL, SIGMA_ABNORMAL)
 doSvm(ABNORMAL_RATE, XMIN, YMIN, XMAX, YMAX)
