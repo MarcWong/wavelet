c2041ab6737ab4b7ec7dbb7accb591ecd1814722
MarcWong <brucewayne@pku.edu.cn>
2019-03-23 21:44:04 +0800

    update script

--- run_si.py
+++ run_si.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 ==========================================
-silicon数据脚本，目前是拆分成了三个：
-先跑process_wavelet.py
-再跑svdd.py
-再跑run_si.py
+silicon数据脚本
 ==========================================
 """
 print(__doc__)
 from pca.plot_pca_wavelet import plot_pca
+from process_wavelet import process_wavelet
+from svm.svdd import svdd
 import numpy as np
 
+i = 0
+LEVEL = 6
 name= ['223-94.npy',
     '223-95.npy',
     '223-96.npy',
@@ -33,7 +34,19 @@ name= ['223-94.npy',
     '223-121-非规律.npy',
 ]
 
-X = np.load('../silicon_data/' + name[0])
-y = np.load('../silicon_data_y/' + name[0])
 
-plot_pca(X, y)
+# 执行小波变换
+input_wavelet = '../silicon/' + name[i]
+output_wavelet = '../silicon_data/' + name[i]
+process_wavelet(input_wavelet, output_wavelet, LEVEL)
+
+
+# 执行SVDD
+input_SVDD = '../silicon_data/' + name[i]
+output_SVDD = '../silicon_data_y/' + name[i]
+svdd(input_SVDD, output_SVDD)
+
+# 执行pca，画图
+# X = np.load('../silicon_data/' + name[i])
+# y = np.load('../silicon_data_y/' + name[i])
+# plot_pca(X, y)
--- svm/svdd.py
+++ svm/svdd.py
@@ -3,40 +3,13 @@
 SVDD
 ==========================================
 """
-print(__doc__)
 
-import random
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.font_manager
 from sklearn import svm
 
-####### 一些参数 #######
-name= ['223-94.npy',
-    '223-95.npy',
-    '223-96.npy',
-    '223-97.npy',
-    '223-100-问题.npy',
-    '223-101.npy',
-    '223-105-问题.npy',
-    '223-106.npy',
-    '223-107-小问题.npy',
-    '223-108.npy',
-    '223-109-断.npy',
-    '223-111-非规律.npy',
-    '223-112-小问题.npy',
-    '223-114-非规律.npy',
-    '223-115-非规律.npy',
-    '223-117.npy',
-    '223-118.npy',
-    '223-119-问题.npy',
-    '223-120.npy',
-    '223-121-非规律.npy',
-]
+def svdd(inputName, outputName):
 
-for i in range(0, len(name)):
-
-    X_train = np.load('../../silicon_data/' + name[i])
+    X_train = np.load(inputName)
 
     # raw_data = np.load('../../data/2544_20160907-13-zs/10.npy')[:, np.array([0,2])]
 
@@ -56,16 +29,19 @@ for i in range(0, len(name)):
     ####### svdd #######
     # fit the model
 
-    clf = svm.OneClassSVM(nu=0.001, kernel="rbf", gamma=0.001)
+    clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
     clf.fit(X_train)
     y_pred_train = clf.predict(X_train)
     # y_pred_test = clf.predict(X_test)
     n_abnormal_train = y_pred_train[y_pred_train == -1].size
     # n_abnormal_test = y_pred_test[y_pred_test == -1].size
 
+    print ("abnormal: %d/%d ;"
+        % (n_abnormal_train, X_train.size))
+
     # print ("abnormal train: %d/%d ; abnormal test: %d/%d ; "
     #     % (n_abnormal_train, train_size, n_abnormal_test, test_size))
 
 
-    print("保存文件: ../../silicon_data_y/" + name[i])
-    np.save("../../silicon_data_y/" + name[i], y_pred_train )
+    print("保存文件: " + outputName)
+    np.save(outputName, y_pred_train )
