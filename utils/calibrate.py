056c9922e5db38adaac342bb377f51d2782683df
MarcWong <brucewayne@pku.edu.cn>
2019-04-05 12:16:42 +0800

    增加calibrate统计

--- utils/calibrate.py
+++ utils/calibrate.py
@@ -4,9 +4,20 @@ def calibrate(input_path, output_path, filename):
     X = np.load(input_path + filename + '.npy')
 
     y_gt = np.zeros(X.shape[0], dtype=int);
-    for i in range(3450,8500):
-        y_gt[i] = 1;
+    m = 0
+    # for i in range(0,1250):
+    #     y_gt[i] = 1
+    #     m += 1
+    for i in range(8346,14577):
+        y_gt[i] = 1
+        m += 1
+    # for i in range(11512,11712):
+    #     y_gt[i] = 1
+    #     m += 1
+    for i in range(13069,19355):
+        y_gt[i] = 1
+        m += 1
 
-    print("保存标定的真实值:" + output_path + filename + '.npy', "异常值范围: 3250-8400")
+    print("保存标定的真实值:" + output_path + filename + '.npy', "测试集异常点数量: ", int(m / 2))
     np.save(output_path + filename + '.npy', y_gt)
     return
